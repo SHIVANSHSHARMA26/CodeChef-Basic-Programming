5953927486833d2dad2f48b4f770683568f3aceb
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-03-18 23:34:31 +0530

    Create Credit score.cpp

---
+++ Credit score.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int score;
+	cin>>score;
+	    if(score>=750){
+	        cout<<"YES";
+	    }
+	    else{
+	        cout<<"NO";
+	    }
+	
+	return 0;
+}
