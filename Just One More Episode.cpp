7c7ee564b03b744fde0fc966ead5f7dc8fdec603
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-03-21 22:12:21 +0530

    Create Just One More Episode.cpp

---
+++ Just One More Episode.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t,start;
+	cin>>t;
+	while(t--){
+	    cin>>start;
+	    if(start>24){cout<<"YES"<<"\n";}
+	    else{cout<<"NO"<<"\n";}
+	}
+	return 0;
+}
