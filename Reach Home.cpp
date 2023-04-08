3ff0f0f35668c9c813b3d2977c6519a5c8338caf
SHIVANSHSHARMA26 <104148493+SHIVANSHSHARMA26@users.noreply.github.com>
2023-04-08 23:16:47 +0530

    Create Reach Home.cpp

---
+++ Reach Home.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t,x,y;
+    cin>>t;
+    while(t--){
+	cin>>x>>y;
+	if(x*5>=y){cout<<"YES"<<"\n";}
+	else{cout<<"NO"<<"\n";}
+	}
+	return 0;
+}
